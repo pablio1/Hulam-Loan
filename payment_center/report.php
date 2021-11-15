6d6c0f144d4aefd38d9fc4be591a5e6ad397f8cc
marty120 <93358934+marty120@users.noreply.github.com>
2021-11-15 17:36:33 +0800

    Update report.php
    Profile picture and menu bar(deleted) updated.

--- payment_center/report.php
+++ payment_center/report.php
@@ -9,6 +9,16 @@ if ($_SESSION['user_type'] != 5) {
 
 ?>
 
+<?php
+$user_id = $_SESSION['user_id'];
+
+$sql = "SELECT * FROM user WHERE user_id =$user_id";
+$query = $dbh->prepare($sql);
+$query->execute();
+$user = $query->fetch();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<!--begin::Head-->
@@ -778,23 +788,7 @@ if ($_SESSION['user_type'] != 5) {
 									<!--end::Dropdown-->
 								</div>
 								<!--end::Notifications-->
-								<!--begin::Quick panel-->
-								<div class="topbar-item mr-1">
-									<div class="btn btn-icon btn-clean btn-lg" id="kt_quick_panel_toggle">
-										<span class="svg-icon svg-icon-xl svg-icon-primary">
-											<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
-											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-													<rect x="0" y="0" width="24" height="24" />
-													<rect fill="#000000" x="4" y="4" width="7" height="7" rx="1.5" />
-													<path d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z" fill="#000000" opacity="0.3" />
-												</g>
-											</svg>
-											<!--end::Svg Icon-->
-										</span>
-									</div>
-								</div>
-								<!--end::Quick panel-->
+								
 								<!--begin::Chat-->
 								<div class="topbar-item">
 									<div class="btn btn-icon btn-clean btn-lg mr-1" data-toggle="modal" data-target="#kt_chat_modal">
@@ -1047,7 +1041,7 @@ if ($_SESSION['user_type'] != 5) {
 				<!--begin::Header-->
 				<div class="d-flex align-items-center mt-5">
 					<div class="symbol symbol-100 mr-5">
-						<div class="symbol-label" style="background-image:url('assets/admin/media/users/icon-company.jpg')"></div>
+						<div class="symbol-label" style="background-image: url(/hulam/assets/keen/payment_center/<?= $user['profile_pic'] ?>"></div>
 						<i class="symbol-badge bg-success"></i>
 					</div>
 					<div class="d-flex flex-column">
@@ -1126,67 +1120,6 @@ if ($_SESSION['user_type'] != 5) {
 		</div>
 		<!-- end::User Panel-->
 
-		<!--begin::Quick Panel-->
-		<div id="kt_quick_panel" class="offcanvas offcanvas-right pt-5 pb-10">
-			<!--begin::Header-->
-			<div class="offcanvas-header offcanvas-header-navs d-flex align-items-center justify-content-between mb-5">
-				<ul class="nav nav-bold nav-tabs nav-tabs-line nav-tabs-line-3x nav-tabs-primary flex-grow-1 px-10" role="tablist">
-					<li class="nav-item">
-						<a class="nav-link" data-toggle="tab" href="#kt_quick_panel_notifications">Notifications</a>
-					</li>
-				</ul>
-				<div class="offcanvas-close mt-n1 pr-5">
-					<a href="#" class="btn btn-xs btn-icon btn-light btn-hover-primary" id="kt_quick_panel_close">
-						<i class="ki ki-close icon-xs text-muted"></i>
-					</a>
-				</div>
-			</div>
-			<!--end::Header-->
-			<!--begin::Content-->
-			<div class="offcanvas-content px-10">
-				<div class="tab-content">
-					<!--begin::Tabpane-->
-						<!--begin::Nav-->
-						<div class="navi navi-icon-circle navi-spacer-x-0">
-							<!--begin::Item-->
-							<a href="#" class="navi-item">
-								<div class="navi-link rounded">
-									<div class="symbol symbol-50 mr-3">
-										<div class="symbol-label">
-											<i class="flaticon-bell text-success icon-lg"></i>
-										</div>
-									</div>
-									<div class="navi-text">
-										<div class="font-weight-bold font-size-lg">5 new user generated report</div>
-										<div class="text-muted">Reports based on sales</div>
-									</div>
-								</div>
-							</a>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<a href="#" class="navi-item">
-								<div class="navi-link rounded">
-									<div class="symbol symbol-50 mr-3">
-										<div class="symbol-label">
-											<i class="flaticon2-box text-danger icon-lg"></i>
-										</div>
-									</div>
-									<div class="navi-text">
-										<div class="font-weight-bold font-size-lg">2 new items submited</div>
-										<div class="text-muted">by Grog John</div>
-									</div>
-								</div>
-							</a>
-							<!--end::Item-->
-						</div>
-						<!--end::Nav-->
-					</div>
-					<!--end::Tabpane-->
-				</div>
-			</div>
-			<!--end::Content-->
-		</div>
-		<!--end::Quick Panel-->
 		<!--begin::Chat Panel-->
 		<div class="modal modal-sticky modal-sticky-bottom-right" id="kt_chat_modal" role="dialog" data-backdrop="false">
 			<div class="modal-dialog" role="document">
